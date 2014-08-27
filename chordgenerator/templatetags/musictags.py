319b1c4b6250eacaac06b0b2713b3a827a30a484
Peter Murphy <peterkmurphy@gmail.com>
2014-08-27 19:09:20 +1000

    Now you can hear the chords.

--- chordgenerator/templatetags/musictags.py
+++ chordgenerator/templatetags/musictags.py
@@ -4,12 +4,48 @@
 # Goal - to provide a nice template tag to represent tables of chords.
 
 from musictheory.musutility import seqtostr;
-from musictheory.temperament import un_unicode_accdtls;
+from musictheory.temperament import un_unicode_accdtls, WestTemp;
 from musictheory.chordgenerator import int_to_roman, scale_chords;
 from django import template;
 from django.utils.safestring import mark_safe
 
 register = template.Library();
+
+# PKM2014 - for each chord listed in the table, we want to get a starting note, and a 
+# list of positions relative to it. For 9th, 11th and 13th chords, we need the position
+# to be adjusted by 12. The reason is that the harmonies for 9th, 11th and 13th
+# sound better with an extra octave.
+
+def get_keyseq_notes_adjusted(temp, note_seq):
+    ourseqnotes = temp.get_keyseq_notes(note_seq);
+    ourseqnotenorm = temp.get_key_of_pos(temp.get_pos_of_key(ourseqnotes[0]))
+    ourseqnotes[0] = ourseqnotenorm
+    ourseqbase = ourseqnotes[1];
+    ourseqlen = len(ourseqbase)
+    snap = None
+    for i in range(ourseqlen):
+        if ((i < (ourseqlen - 1)) and (ourseqbase[i+1] < ourseqbase[i])):
+            snap = i + 1;
+            break;
+    if not snap:
+        return ourseqnotes
+    else:
+        ournewseq = ourseqbase;
+        for i in range(ourseqlen):
+            if i >= snap:
+                ournewseq[i] = ournewseq[i] + temp.no_keys;
+        return [ourseqnotes[0], ournewseq]
+
+
+def getkeyseqid(keyseqnotes):
+    """ Expressed as a id """
+    ourreturn = keyseqnotes[0] + '-';
+    ourlen = len(keyseqnotes[1]);
+    for i, v in enumerate(keyseqnotes[1]):
+        ourreturn += str(v);
+        if i != ourlen - 1:
+            ourreturn += '-';
+    return ourreturn;
 
 # Note: this is a copy of chordgentable from musictheory. This method should belong here.
 
@@ -26,19 +62,44 @@ def chordgentableneu(scales):
         thestring += "<caption>%s</caption>\n" % (scale.key + " " + scale.full_name +
             ": " + seqtostr(scale.full_notes));
         thestring += "<thead>\n" + startrow + "<th class=\"text-center\">Chord Types</th>\n";
-        for q in range(7):
-            thestring += "<th class=\"text-center\">%s</th>\n" % str(int_to_roman(q+1));
+        
+# PKM2014 - originally:       
+        
+#        for q in range(7):
+#            thestring += "<th class=\"text-center\">%s</th>\n" % str(int_to_roman(q+1));
+            
+# Now
+        basedone = False;    
+        for i in scale.full_notes:
+            ourseqnoteadj = get_keyseq_notes_adjusted(WestTemp, [i]);
+            ourseqid = getkeyseqid(ourseqnoteadj);
+            if basedone:
+                thestring += "<th class=\"text-center chordy\" id=\"%s-0\"><p>%s</p></th>\n" % (ourseqid, i);
+            else:
+                thestring += "<th class=\"text-center basedone chordy\" id=\"%s-0\"><p>%s</p></th>\n" % (ourseqid, i);
+                basedone = True;
+            
         thestring += endrow + "</thead>\n<tbody>\n";
+        
+# Adding new row for base notes.        
+#        
+#        thestring += startrow; 
+#        thestring += "<td>Base note</td>\n";
+#        for i in scale.full_notes:
+#            thestring += "<td>%s</td>\n" % (i);
+#        thestring += endrow;
 
         for i in scale.rows:
             thestring += startrow;    
             thestring += "<td>%s</td>\n" % i.chord_type;
             for j in i.notes:
+                ourseqnoteadj = get_keyseq_notes_adjusted(WestTemp, j.notes);
+                ourseqid = getkeyseqid(ourseqnoteadj);
                 if not j.chordname_1:
-                    thestring += ("<td><p>%s<br />" % (unicode(j.chordname_1)));
+                    thestring += ("<td class=\"chordy\" id = \"%s\" ><p>%s<br />" % (ourseqid, unicode(j.chordname_1)));
                     thestring += ("<i>%s</i><br />" % (unicode(j.chordname_2)));
                 else:
-                    thestring += ("<td><p>%s<br />" % (j.notes[0]+" "+unicode(j.chordname_1)));
+                    thestring += ("<td class=\"chordy\" id = \"%s\"><p>%s<br />" % (ourseqid, j.notes[0]+" "+unicode(j.chordname_1)));
                     thestring += ("<i>%s</i><br />" % (j.notes[0]+unicode(j.chordname_2)));
                     
                 thestring += ("<b>%s</b></p></td>" % seqtostr(j.notes));                
@@ -55,6 +116,7 @@ def chordtabletag(value, arg = False):
     The function is really a wrapper around chordgentable. The arg argument
     indicates whether it is ASCII-only (True) or Unicode (False).
     '''
+    output = chordgentableneu([value]);
     try:
         
 # Any exception - any at all - returns an empty string.
